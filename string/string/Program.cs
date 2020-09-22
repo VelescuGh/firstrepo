a18a2755d714dd3cbb5a0b05f490a23284fc32f8
VelescuGh <velescu.gheorghe93@gmail.com>
2020-09-22 17:51:19 +0300

    test

--- string/string/Program.cs
+++ string/string/Program.cs
@@ -12,7 +12,7 @@ namespace @string
         static void Main(string[] args)
         {
 
-            string myString = "Hello Batman";
+            string myString = "Hello Bat  man";
             string mickey = "Hello Mickey Mouse";
             if (myString==mickey)
             {
